b46c09a1b197ed97d8d3f4137453c50a8e4bf5e7
phandb72 <phandb72@gmail.com>
2022-05-10 07:06:48 -0500

    Install and Setup Alembic

---
+++ migrations/versions/391765a4a04d_initial_migrations.py
@@ -0,0 +1,24 @@
+"""Initial Migrations
+
+Revision ID: 391765a4a04d
+Revises: 
+Create Date: 2022-05-09 23:56:29.718766
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '391765a4a04d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
